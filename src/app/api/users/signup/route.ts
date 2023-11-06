f3e2d72f315aeadf0eed06459b9336179ee6ceb9
ankushdev <ankush@gmail.com>
2023-11-06 23:29:21 +0530

    uncomment user check

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -15,12 +15,12 @@ export async function POST(request: NextRequest) {
 
         console.log(reqBody);
 
-        //check if user already exists
-        // const user = await User.findOne({ email })
+        // check if user already exists
+        const user = await User.findOne({ email })
 
-        // if (user) {
-        //     return NextResponse.json({ error: "User already exists" }, { status: 400 })
-        // }
+        if (user) {
+            return NextResponse.json({ error: "User already exists" }, { status: 400 })
+        }
 
         //hash password
         const salt = await bcryptjs.genSalt(10)
