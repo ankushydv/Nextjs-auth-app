045acc3f9ad8205ce7dc1b455788773332411347
ankushdev <ankush@gmail.com>
2023-11-06 23:28:57 +0530

    login route chng of jwt and set cookies r done

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,54 @@
+import { connect } from "@/dbconfig/dbconfig";
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connect();
+
+export async function Post(request: NextRequest) {
+  try {
+    const reqBody = request.json();
+    console.log(reqBody);
+    const { email, password }: any = reqBody;
+
+    //Check user exist or not.
+    const user = await User.findOne({ email });
+
+    // ..iff user did not exist then send a response with error
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exist" },
+        { status: 400 }
+      );
+    }
+    //..if user exist then validate a password
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invaild Password" }, { status: 400 });
+    }
+
+    //create token data for jwt first variable set user info
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const secret = process.env.SECRET_TOKEN;
+
+    const token = await jwt.sign(tokenData, secret!, { expiresIn: "1d" });
+
+    const response = NextResponse.json({
+      message: "Login Successful",
+      success: true,
+    });
+
+    response.cookies.set("token", token, { httpOnly: true });
+
+    return response;
+  } catch (error: any) {
+    console.log("Something went in backend", `error:${error}`);
+    NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
